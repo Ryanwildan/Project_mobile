9f55f83064d31bbb55e14177fcddd22df9bc7bcc
SufardiMadoa <Sufardimpv@gmail.com>
2024-05-05 17:19:16 +0700

    feat/done

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,210 @@
 import 'package:flutter/material.dart';
+import 'ImageTile.dart'; // Import file ImageTile.dart
+import 'SignInPage2.dart'; // Import file ImageTile.dart
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: SignInPage2(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class OnboardingPage1 extends StatelessWidget {
+  const OnboardingPage1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingPagePresenter(pages: [
+      OnboardingPageModel(
+        title: 'Fast, Fluid and Secure',
+        description: 'Enjoy the best of the world in the palm of your hands.',
+        imageUrl: 'https://i.ibb.co/cJqsPSB/scooter.png',
+        bgColor: Colors.indigo,
+      ),
+      OnboardingPageModel(
+        title: 'Connect with your friends.',
+        description: 'Connect with your friends anytime anywhere.',
+        imageUrl: 'https://i.ibb.co/LvmZypG/storefront-illustration-2.png',
+        bgColor: const Color(0xff1eb090),
+      ),
+      OnboardingPageModel(
+        title: 'Bookmark your favourites',
+        description:
+            'Bookmark your favourite quotes to read at a leisure time.',
+        imageUrl: 'https://i.ibb.co/420D7VP/building.png',
+        bgColor: const Color(0xfffeae4f),
+      ),
+      OnboardingPageModel(
+        title: 'Follow creators',
+        description: 'Follow your favourite creators to stay in the loop.',
+        imageUrl: 'https://i.ibb.co/cJqsPSB/scooter.png',
+        bgColor: Colors.purple,
+      ),
+    ]);
+  }
+}
+
+class OnboardingPagePresenter extends StatefulWidget {
+  final List<OnboardingPageModel> pages;
+  final VoidCallback? onSkip;
+  final VoidCallback? onFinish;
+
+  const OnboardingPagePresenter(
+      {Key? key, required this.pages, this.onSkip, this.onFinish})
+      : super(key: key);
+
+  @override
+  State<OnboardingPagePresenter> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPagePresenter> {
+  // Store the currently visible page
+  int _currentPage = 0;
+  // Define a controller for the pageview
+  final PageController _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const SingleChildScrollView(
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        color: widget.pages[_currentPage].bgColor,
+        child: SafeArea(
           child: Column(
             children: [
-              ImageSection(
-                image: 'images/mobil.jpg',
+              Expanded(
+                // Pageview to render each page
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: widget.pages.length,
+                  onPageChanged: (idx) {
+                    // Change current page when pageview changes
+                    setState(() {
+                      _currentPage = idx;
+                    });
+                  },
+                  itemBuilder: (context, idx) {
+                    final item = widget.pages[idx];
+                    return Column(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: Image.network(
+                              item.imageUrl,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Column(children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(item.title,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6
+                                        ?.copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color: item.textColor,
+                                        )),
+                              ),
+                              Container(
+                                constraints:
+                                    const BoxConstraints(maxWidth: 280),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 24.0, vertical: 8.0),
+                                child: Text(item.description,
+                                    textAlign: TextAlign.center,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText2
+                                        ?.copyWith(
+                                          color: item.textColor,
+                                        )),
+                              )
+                            ]))
+                      ],
+                    );
+                  },
+                ),
               ),
-              TitleSection(name: 'NISSAN GTR R34', location: 'TOKYO,JAPAN'),
-              ButtonSection(),
-              TextSection(
-                description:
-                    'Nissan GT-R R34, dikenal sebagai ikon dalam dunia otomotif performa, adalah mobil sport Jepang yang legendaris. '
-                    'Diperkenalkan pada akhir tahun 1990-an, mobil ini segera mendapat reputasi sebagai salah satu yang paling kuat dan gesit di jalanan dan lintasan balap. '
-                    'Dilengkapi dengan mesin twin-turbocharged 2.6 liter inline-6, '
-                    'Nissan GT-R R34 menawarkan kombinasi tenaga dan kelincahan yang luar biasa. ',
+
+              // Current page indicator
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: widget.pages
+                    .map((item) => AnimatedContainer(
+                          duration: const Duration(milliseconds: 250),
+                          width: _currentPage == widget.pages.indexOf(item)
+                              ? 30
+                              : 8,
+                          height: 8,
+                          margin: const EdgeInsets.all(2.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ))
+                    .toList(),
               ),
+
+              // Bottom buttons
+              SizedBox(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            visualDensity: VisualDensity.comfortable,
+                            foregroundColor: Colors.white,
+                            textStyle: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          widget.onSkip?.call();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PinterestGrid()),
+                          );
+                        },
+                        child: const Text("Skip")),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          visualDensity: VisualDensity.comfortable,
+                          foregroundColor: Colors.white,
+                          textStyle: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        if (_currentPage == widget.pages.length - 1) {
+                          widget.onFinish?.call();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PinterestGrid()),
+                          );
+                        } else {
+                          _pageController.animateToPage(_currentPage + 1,
+                              curve: Curves.easeInOutCubic,
+                              duration: const Duration(milliseconds: 250));
+                        }
+                      },
+                      child: Row(
+                        children: [
+                          Text(
+                            _currentPage == widget.pages.length - 1
+                                ? "Finish"
+                                : "Next",
+                          ),
+                          const SizedBox(width: 8),
+                          Icon(_currentPage == widget.pages.length - 1
+                              ? Icons.done
+                              : Icons.arrow_forward),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -37,152 +213,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 61, 3, 237),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Icon(
-            Icons.verified,
-            color: Color.fromARGB(255, 0, 145, 255),
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
-    return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ButtonWithText(
-            color: color,
-            icon: Icons.call,
-            label: 'CALL',
-          ),
-          ButtonWithText(
-            color: color,
-            icon: Icons.near_me,
-            label: 'ROUTE',
-          ),
-          ButtonWithText(
-            color: color,
-            icon: Icons.share,
-            label: 'SHARE',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({
-    super.key,
-    required this.description,
-  });
-
+class OnboardingPageModel {
+  final String title;
   final String description;
+  final String imageUrl;
+  final Color bgColor;
+  final Color textColor;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        description,
-        softWrap: true,
-      ),
-    );
-  }
-}
-
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
-
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(
-      image,
-      width: 600,
-      height: 240,
-      fit: BoxFit.cover,
-    );
-  }
+  OnboardingPageModel(
+      {required this.title,
+      required this.description,
+      required this.imageUrl,
+      this.bgColor = Colors.blue,
+      this.textColor = Colors.white});
 }
